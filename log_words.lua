59f885e5c141cddd4f83b3be9d990589492dfd22
聆听·彼岸 <38424427+jnuse@users.noreply.github.com>
2024-08-16 21:08:57 +0800

    Update log_words.lua

--- log_words.lua
+++ log_words.lua
@@ -1,8 +1,6 @@
 
--- 定义常见标点符号的集合
-local punctuation_set = ".,!?;:\"'()[]{}<>@#&*%-_+=|/\\~`1234567890，。《》：；‘’“”"
+local punctuation_set = ".,!?;:\"'()[]{}<>@#&*%-_+=|/\\~`1234567890，。《》：；‘’“”！（）【】「」、？abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
--- 判断字符是否为标点符号
 function not_punctuation(char)
     return string.find(punctuation_set, char, 1, true) == nil
 end
@@ -10,15 +8,17 @@ end
 function on_commit(context)
     local commit_text = context:get_commit_text()
 
-    -- 指定保存文件的绝对路径，相对路径会报错
+    -- 指定保存文件的绝对路径，不能使用相对路径，否则会报错
     local file_path = "D:\\rime\\weasel-0.16.1\\input_log_word.txt"
 
-    local file = io.open(file_path, "a")
-    if file and not_punctuation(commit_text) then
-        file:write(commit_text .. "\n")
-        file:close()
+    local file, err  = io.open(file_path, "a")
+    if file then
+        if not_punctuation(commit_text) then
+            file:write(commit_text .. "\n")
+            file:close()
+        end
     else
-        log.error("无法打开文件进行写入: " .. file_path)
+        log.error("无法打开文件进行写入: " .. err)
     end
 
 end
